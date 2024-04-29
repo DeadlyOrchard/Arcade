380b738c4d58cd0ef7a91d1aa5fd6daf0bf8a60d
DeadlyOrchard <natelwhite01@gmail.com>
2024-04-28 21:56:22 -0500

    First commit for this repository. I'll be using SDL2 to create a number of little arcade games.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+        return 0;
+    }
+    printf("SDL successfully initialized\n");
+    SDL_Quit();
+    return 0;
+}
